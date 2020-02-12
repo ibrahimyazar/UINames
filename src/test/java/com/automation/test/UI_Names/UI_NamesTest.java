32f31ea02f14f542e5ff96896dfcaa5eab4586a1
ibrahim Yazar <ibrahimyazarusa@gmail.com>
2020-02-12 04:42:57 -0500

    addede new test cases

--- src/test/java/com/automation/test/UI_Names/UI_NamesTest.java
+++ src/test/java/com/automation/test/UI_Names/UI_NamesTest.java
@@ -45,17 +45,9 @@ No params test
     assertEquals(200, response.statusCode());
     assertEquals("application/json; charset=utf-8", response.contentType());
     response.then().assertThat().body("name", not(empty()));
-
-
-    String info = response.getBody().asString();
-
-    System.out.println(info);
-//    for (String Infos : info) {
-//        System.out.println(Infos);
-//
-//    }
-
-    //assertTrue(response.body(),);
+    response.then().assertThat().body("surname", not(empty()));
+    response.then().assertThat().body("region", not(empty()));
+    response.then().assertThat().body("gender", not(empty()));
 
 }
 
@@ -77,10 +69,58 @@ Gender test
     assertEquals("application/json; charset=utf-8", response.contentType());
     response.then().assertThat().body("gender", is(gender));
     String info = response.getBody().asString();
+    String genders = response.jsonPath().getString("gender");
+    System.out.println(genders);
     System.out.println(info);
 
 }
 
+/*
+2 params test
+1. Create a request by providing query parameters: a valid region and gender NOTE: Available region values are given in the documentation
+2. Verify status code 200, content type application/json; charset=utf-8
+3. Verify that value of gender field is same from step 1
+4. Verify that value of region field is same from step 1
+ */
+
+    @Test
+    @DisplayName("Verify that value of region field is same from step 1")
+    public void Two_Params_Test(){
+
+        String region = "Greece";
+        String gender = "female";
+
+        Response response = given()
+                                .accept(ContentType.JSON)
+                                .queryParam("region", region)
+                                .queryParam("gender", gender)
+                            .when()
+                                .get().prettyPeek();
+        response.then().assertThat().statusCode(200).
+                                    contentType("application/json; charset=utf-8").
+                                    body("gender", is(gender))
+                                    .body("region", is(region));
+    }
+
+    /*
+    Invalid gender test
+1. Create a request by providing query parameter: invalid gender
+2. Verify status code 400 and status line contains Bad Request
+3. Verify that value of error field is "Invalid gender"
+     */
+    @Test
+    @DisplayName("Verify that value of error field is Invalid gender")
+    public void invalid_Gender_Test(){
+
+        Response response = given()
+                                .accept(ContentType.JSON)
+                                .queryParam("gender", "invalid")
+                            .get();
+        assertEquals(400, response.statusCode());
+        assertTrue( response.statusLine().contains("Bad Request"));
+        assertEquals("Invalid gender", response.jsonPath().getString("error"));
+        System.out.println(response.jsonPath().getString("error"));
+    }
 
 
 }
